c02ac44e2f94b4c0c8f9579778d319b85dcf7ec0
Shawn Koh <shawn@shawnkoh.sg>
2022-01-23 19:07:24 +0800

    Add Store.forEach

---
+++ Sources/ComposableArchitecture/UIKit/ForEachUIKit.swift
@@ -0,0 +1,24 @@
+import Combine
+
+extension Store {
+    public func forEach<EachState, EachAction, ID> (
+        _ body: @escaping (Store<EachState, EachAction>) -> Void
+    ) -> Cancellable where ID: Hashable, State == IdentifiedArray<ID, EachState>, Action == (ID, EachAction) {
+        self.state
+            .removeDuplicates(by: { $0.ids == $1.ids})
+            .sink { state in
+                state.ids.forEach { id in
+                    var element = self.state.value[id: id]!
+                    body(
+                        self.scope(
+                            state: {
+                                element = $0[id: id] ?? element
+                                return element
+                            },
+                            action: { (id, $0) }
+                        )
+                    )
+                }
+            }
+    }
+}
