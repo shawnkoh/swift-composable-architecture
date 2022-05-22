d52f4da81da2c451d643e376bce1413fb817de2a
Shawn Koh <shawn@shawnkoh.sg>
2022-05-22 17:09:47 +0800

    Add forEach overload

--- Sources/ComposableArchitecture/UIKit/ForEachUIKit.swift
+++ Sources/ComposableArchitecture/UIKit/ForEachUIKit.swift
@@ -21,4 +21,28 @@ extension Store {
                 }
             }
     }
+
+    public func forEach<EachState, EachAction, ID> (
+        onChange: @escaping () -> Void,
+        _ body: @escaping (Store<EachState, EachAction>) -> Void
+    ) -> Cancellable where ID: Hashable, State == IdentifiedArray<ID, EachState>, Action == (ID, EachAction) {
+        self.state
+            .removeDuplicates(by: { $0.ids == $1.ids})
+            .sink { state in
+                onChange()
+                state.ids.forEach { id in
+                    var element = self.state.value[id: id]!
+                    body(
+                        self.scope(
+                            state: {
+                                element = $0[id: id] ?? element
+                                return element
+                            },
+                            action: { (id, $0) }
+                        )
+                    )
+                }
+            }
+    }
+
 }
